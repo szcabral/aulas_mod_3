174cfbcbd4f51d93b1ab02250f3f918446579420
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-08-20 20:08:42 +0000

    fix: arrumando o teste para o calculo.

--- entrega_1/projeto_1/main.py
+++ entrega_1/projeto_1/main.py
@@ -5,13 +5,12 @@ print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
 print("1° Teste: lista [0,1,2,3,4,5,6,7,8]")
 try:
-        result = mean_var_std.calculate([0,1,2,3,4,5,6,7,8])
+    result = mean_var_std.calculate([0,1,2,3,4,5,6,7,8])
     print("Resultado:")
     for key, value in result.items():
-        print(f"  '{key}': {value}")
-         print("Teste realizado com sucesso!\n")
+        print(f"'{key}': {value}")
+    print("Teste realizado com sucesso!\n")
 except Exception as e:
     print(f"Foi encontrado um erro: {e}\n")
-
 
 main(module='test_module', exit=False)
