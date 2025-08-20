a67dcc8cbafc134d2a1664ce1b5ddd0db9c1d5a0
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-08-20 20:06:42 +0000

    fix: arrumando os erros do código.

--- entrega_1/projeto_1/mean_var_std.py
+++ entrega_1/projeto_1/mean_var_std.py
@@ -1,42 +1,42 @@
 import numpy as np
 
 def calculate(list):
-
-    if len(numbers) != 9:
+    if len(list) != 9:
         raise ValueError("A lista deve conter nove números")
 
-    matrix = np.array(list_input).reshape(3, 3)
+    matrix = np.array(list).reshape(3, 3)
 
-calculations = {
-    'mean': [
-        matrix.mean(axis=0).tolist(),
-        matrix.mean(axis=1).tolist(),
-        matrix.mean()
-    ],
-      'variance': [
+    calculations = {
+        'mean': [
+            matrix.mean(axis=0).tolist(),
+            matrix.mean(axis=1).tolist(),
+            matrix.mean().tolist()
+        ],
+        'variance': [
             matrix.var(axis=0).tolist(),
             matrix.var(axis=1).tolist(),
-            matrix.var().item()
+            matrix.var().tolist()
         ],
         'standard deviation': [
             matrix.std(axis=0).tolist(),
             matrix.std(axis=1).tolist(),
-            matrix.std().item()
+            matrix.std().tolist()
         ],
         'max': [
             matrix.max(axis=0).tolist(),
             matrix.max(axis=1).tolist(),
-            matrix.max().item()
+            matrix.max().tolist()
         ],
         'min': [
             matrix.min(axis=0).tolist(),
             matrix.min(axis=1).tolist(),
-            matrix.min().item()
+            matrix.min().tolist()
         ],
         'sum': [
             matrix.sum(axis=0).tolist(),
             matrix.sum(axis=1).tolist(),
-            matrix.sum().item()
-    ]
-}
+            matrix.sum().tolist()
+        ]
+    }
+
     return calculations
