33a3369161ab667df0e0a466b2dbe89509c46be7
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-08-22 16:07:18 +0000

    feat: adicionando respostas das perguntas até a quarta pergunta.

--- entrega_1/projeto_2/demographic_data_analyzer.py
+++ entrega_1/projeto_2/demographic_data_analyzer.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df["sex"] == "Male", "age"].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    num_bachelors = df.loc[df["education"] == "Bachelors"].shape[0]
+    percentage_bachelors = round((num_bachelors / df.shape[0])* 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
