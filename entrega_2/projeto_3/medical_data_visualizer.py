18bf71787d032ec43536a480475b6ef208f1b2c4
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-09-05 17:33:41 +0000

    feat: adicionando heat_map.

--- entrega_2/projeto_3/medical_data_visualizer.py
+++ entrega_2/projeto_3/medical_data_visualizer.py
@@ -24,38 +24,37 @@ def draw_cat_plot():
     df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
     df_cat = df_cat.rename(columns={"value": "total"})
     # 7
-
-
+    fig = sns.catplot(x="variable", y="total", hue="value", col="cardio", data=df_cat, kind="bar")
 
     # 8
-    fig = None
-
 
     # 9
-    fig.savefig('catplot.png')
     return fig
 
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df["ap_lo"] <= df["ap_hi"]) &
+        (df["height"] >= df["height"].quantile(0.025)) &
+        (df["height"] <= df["height"].quantile(0.975)) &
+        (df["weight"] >= df["weight"].quantile(0.025)) &
+        (df["weight"] <= df["weight"].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(corr)
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", linewidths=.5, cbar_kws={"shrink": 0.5}, square=True, ax=ax)
 
 
     # 16
-    fig.savefig('heatmap.png')
     return fig
