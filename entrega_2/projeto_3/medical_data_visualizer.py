22e190899baa00d9dc79fe9f67b0199c389c2e7a
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-09-05 17:08:44 +0000

    feat: adicionando atividade 3, 4, 5 e 6 .

--- entrega_2/projeto_3/medical_data_visualizer.py
+++ entrega_2/projeto_3/medical_data_visualizer.py
@@ -12,18 +12,17 @@ df["bmi"] = (df["weight"] / df["height"] )** 2
 df['overweight'] = (df["bmi"] > 25).astype(int)
 
 # 3
-
+df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
+df["gluc"] = (df["gluc"] > 1).astype(int)
 
 # 4
-def draw_cat_plot():
+def draw_cat_plot(): 
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
 
     # 6
-    df_cat = None
-    
-
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
+    df_cat = df_cat.rename(columns={"value": "total"})
     # 7
 
 
